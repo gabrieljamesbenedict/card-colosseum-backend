cd7540c858fa6f494fc7830537e9c990d76b9ed9
Gabriel James Benedict <benok.loslos@gmail.com>
2026-01-30 18:38:48 +0800

    fix: replace wrong import of @Id in Card entity class

--- src/main/java/com/porado/card_colosseum/backend/entity/Card.java
+++ src/main/java/com/porado/card_colosseum/backend/entity/Card.java
@@ -1,11 +1,7 @@
 package com.porado.card_colosseum.backend.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.annotation.Id;
 
 @Entity
 @RequiredArgsConstructor
